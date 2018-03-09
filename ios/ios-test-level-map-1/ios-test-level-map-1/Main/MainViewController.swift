16342c72dc3a744084c1dfd54a84f2d0146f3361
Louis Foster <123@louisfoster.com>
2018-03-10 00:53:15 +1100

    display basic world on screen

---
+++ ios/ios-test-level-map-1/ios-test-level-map-1/Main/MainViewController.swift
@@ -0,0 +1,44 @@
+//
+//  MainViewController.swift
+//  ios-test-level-map-1
+//
+//  Created by Louie on 9/3/18.
+//  Copyright © 2018 Louis Foster. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+
+class MainViewController: UIViewController {
+    
+    @IBOutlet
+    private var sceneView: SCNView?
+
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        let scene = SCNScene()
+        
+        let map = Map()
+        scene.rootNode.addChildNode(map)
+        
+        let camera = SCNNode()
+        camera.camera = SCNCamera()
+        camera.position = SCNVector3(x: 0, y: 10, z: 0)
+        camera.look(at: SCNVector3(x: 0, y: 0, z: 0))
+        scene.rootNode.addChildNode(camera)
+        
+        let ambientLightNode = SCNNode()
+        ambientLightNode.light = SCNLight()
+        ambientLightNode.light!.type = .ambient
+        ambientLightNode.light!.color = UIColor.darkGray
+        scene.rootNode.addChildNode(ambientLightNode)
+        
+        self.sceneView?.allowsCameraControl = true
+        self.sceneView?.showsStatistics = true
+        self.sceneView?.backgroundColor = UIColor.black
+        
+        self.sceneView?.scene = scene
+    }
+}
